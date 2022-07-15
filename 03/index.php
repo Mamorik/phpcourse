75268240d086dc17104e5f60f23ec0f90c64f305
Sergey Smolkin <honir220@gmail.com>
2022-07-15 13:57:02 +0300

    Comments in PHP

--- 03/index.php
+++ 03/index.php
@@ -1,3 +1,6 @@
+<?php //phpinfo();
+// Однострочный комментарий
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -5,14 +8,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Урок 2</title>
+    <title>Урок 3</title>
 </head>
 <body>
+
+<!-- PHP -->
+
     <p> Hello </p>
     <?php
+
+    /*
+    *Многострочный
+    *комментарий
+    */
         echo '<p> Hello2 </p>';
-        echo '<p> Hello3 </p>';
-        echo '<p> Hello4 </p>';
+        //echo '<p> Hello3 </p>';
+        echo '<p> Hello4 </p>'; #Комментарий в стиле Unix
     ?>
+
+    <?//= '<p> Hello5 </p>'; ?>
+
+    <? echo '<p> Hello6 </p>'; ?>
 </body>
 </html>
